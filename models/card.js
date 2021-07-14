b844958b8a7c6d5e6c8ea2f0874758d146b98c47
Andrey Aryamnov <shon9000000@yandex.ru>
2021-07-14 09:48:58 +0700

    First commit

---
+++ models/card.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const ObjectId = require('mongodb').ObjectID;
+// Опишем схему:
+const cardSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  link: {
+    type: String,
+    required: true,
+  },
+  owner: {
+    type: ObjectId,
+    required: true,
+  },
+  likes: {
+    type: ObjectId,
+    default: {},
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+// создаём модель и экспортируем её
+module.exports = mongoose.model('card', cardSchema);
